4ee47be8148ce2312de7326aa968965670939667
Fuad2e3 <114542594+Fuad2e3@users.noreply.github.com>
2023-10-11 13:29:18 +0600

    Update

--- Data Structure/Array/SearchingAlgorithm.cpp
+++ Data Structure/Array/SearchingAlgorithm.cpp
@@ -1,1 +1,30 @@
-
+#include<iostream>
+using namespace std;
+ 
+int main()
+{
+int i,s_it,size,location=1;
+cout<<"Enter array size";
+cin>>size;
+ 
+int a[size];
+ 
+srand(time(0));
+for(int i=0; i<size; i++){
+a[i]=rand()%50;
+}
+ 
+for(int i=0; i<size; i++){
+cout<<a[i]<<" ";
+}
+cin>>s_it;
+while(a[location] !=s_it  && location<=size )
+location++;
+if(location==size+1)
+cout<<"Not found";
+else
+cout<<location<<"is the location of the item"<<s_it;
+cout<<endl;
+ 
+}
+ 

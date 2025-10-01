f3519d636f1eb6f09a84fe7bf5328a1a175bb22c
MD. ABDULLAH AL FUAD <114542594+Fuad2e3@users.noreply.github.com>
2025-10-01 19:29:21 +0600

    Add files via upload

---
+++ Artificial-intelligence-Lab/odd even.py
@@ -0,0 +1,8 @@
+num = 2, 3, 4, 5, 6
+
+for n in num:
+    if n % 2 == 0:
+        print(n, "is even")
+    else:
+        print(n, "is odd")
+        
---
+++ Artificial-intelligence-Lab/secend hight.py
@@ -0,0 +1,5 @@
+numbers = 12, 7, 9, 20, 33, 40
+
+numbers.sort()   
+second_highest = numbers[-2]   
+print("Second highest number is =", second_highest)

56ee0bce68a78521b2947bd38a2416288dc2f0c8
MD. ABDULLAH AL FUAD <114542594+Fuad2e3@users.noreply.github.com>
2025-10-11 00:15:13 +0600

    Add files via upload

---
+++ Artificial-intelligence-Lab/Range of Indexes.py
@@ -0,0 +1,8 @@
+tuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango", "grape")
+
+b = tuple[3]
+
+l = tuple[-4]
+
+print("4th element from beginning:", b)
+print("4th element from last:", l)
